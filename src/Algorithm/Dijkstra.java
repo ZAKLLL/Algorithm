57e9053eee896c31688a624a22f02d54a26fbbc8
zhangjiakui <zjk19971225@gmail.com>
2020-07-12 21:10:41 +0800

    添加dijkstra算法

---
+++ src/Algorithm/Dijkstra.java
@@ -0,0 +1,55 @@
+package Algorithm;
+
+import javafx.util.Pair;
+
+import java.util.*;
+
+/**
+ * @Classname Dijkstra
+ * @Description Dijkstra算法模板
+ * @Date 2020/7/12 20:44
+ * @Created by zhangjiakui
+ */
+public class Dijkstra {
+
+    HashMap<Character, HashMap<Character, Integer>> graph = new HashMap<>();
+
+    public HashMap<Character, Integer> init_distance(HashMap<Character, HashMap<Character, Integer>> graph, Character s) {
+        HashMap<Character, Integer> distance = new HashMap<>();
+        distance.put(s, 0);
+        for (Character vertex : graph.keySet()) {
+            if (s != vertex) {
+                distance.put(vertex, Integer.MAX_VALUE);
+            }
+        }
+        return distance;
+    }
+
+    public HashMap<Character, Integer> dijkstra(HashMap<Character, HashMap<Character, Integer>> graph, Character s) {
+        Queue<Pair<Integer, Character>> pq = new PriorityQueue<>(Comparator.comparingInt(Pair::getKey));
+        pq.add(new Pair<>(0, s));
+        Set<Character> seen = new HashSet<>();
+        HashMap<Character, Character> parent = new HashMap<>();
+        HashMap<Character, Integer> distance = init_distance(graph, s);
+        while (!pq.isEmpty()) {
+            Pair<Integer, Character> poll = pq.poll();
+            int dist = poll.getKey();
+            Character vertex = poll.getValue();
+            seen.add(vertex);
+            Set<Character> nodes = graph.get(vertex).keySet();
+            for (Character w : nodes) {
+                if (!seen.contains(w)) {
+                    int curDist = dist + graph.get(vertex).get(w);
+                    if (curDist < distance.get(w)) {
+                        distance.put(w, curDist);
+                        pq.add(new Pair<>(curDist, w));
+                        parent.put(w, vertex);
+                    }
+                }
+            }
+        }
+        return distance;
+    }
+
+
+}
--- src/Solution.java
+++ src/Solution.java
@@ -2,6 +2,7 @@ import Algorithm.Utils;
 import DataStructure.TreeNode;
 import javafx.print.PageOrientation;
 import jdk.nashorn.internal.ir.WhileNode;
+import org.omg.CORBA.DataOutputStream;
 import org.omg.CORBA.INTERNAL;
 
 import java.lang.reflect.Array;
@@ -20,33 +21,85 @@ public class Solution {
     //    int[][] dir = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     int[] dir = new int[]{1, 0, -1, 0, 1};
 
-    public boolean isPathCrossing(String path) {
-        Set<List<Integer>> set = new HashSet<>();
-        int a = 0, b = 0;
-        for (int i = 0; i < path.length(); i++) {
-            ArrayList<Integer> objects = new ArrayList<>();
-            objects.add(a);
-            objects.add(b);
-            if (set.contains(objects)) return true;
-            set.add(objects);
-            char c = path.charAt(i);
-            if (c == 'N') {
-                a++;
-            } else if (c == 'S') {
-                a--;
-            } else if (c == 'E') {
-                b++;
-            } else b--;
+    public String reformatDate(String date) {
+        String[] s2 = new String[]{"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        Map<String, String> map = new HashMap<>();
+        int i = 0;
+        for (String s : s2) {
+            i++;
+            map.put(s, i < 10 ? "0" + i : i + "");
         }
-        ArrayList<Integer> objects = new ArrayList<>();
-        objects.add(a);
-        objects.add(b);
-        return set.contains(objects);
+        String[] s = date.split(" ");
+        String substring = s[0].substring(0, s[0].length() - 2);
+        return s[2] + "-" + map.get(s[1]) + "-" + (substring.length() < 2 ? "0" + substring : substring);
+    }
+
+    public int rangeSum(int[] nums, int n, int left, int right) {
+        int res = 0;
+        Queue<Integer> pq = new PriorityQueue<>();
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            List<Integer> tmpList = new ArrayList<>();
+            for (Integer integer : list) {
+                tmpList.add(num + integer);
+                pq.add(num + integer);
+            }
+            tmpList.add(num);
+            pq.add(num);
+            list.clear();
+            list.addAll(tmpList);
+            tmpList.clear();
+        }
+        for (int i = 0; i < left - 1; i++) {
+            pq.poll();
+        }
+        for (int i = 0; i < right - left + 1; i++) {
+            res += pq.poll() % 1000000007;
+        }
+        return res;
+    }
+
+    public int minDifference(int[] nums) {
+        int res = Integer.MAX_VALUE;
+        Arrays.sort(nums);
+        if (nums.length <= 3) return 0;
+        int n = nums.length;
+        res = Math.min(nums[n - 4] - nums[0], res);
+        res = Math.min(nums[n - 1] - nums[3], res);
+        res = Math.min(nums[n - 3] - nums[1], res);
+        return Math.min(nums[n - 2] - nums[2], res);
+    }
+
+
+    public boolean winnerSquareGame(int n) {
+        return help(n, true);
+    }
+
+    public boolean help(int a, boolean f) {
+        if (isSq(a)) {
+            return f;
+        }
+        int a1 = a;
+        List<Integer> integers = new ArrayList<>();
+        while (a1 > 0) {
+            if (isSq(a1)) {
+                integers.add(a1);
+            }
+            a1--;
+        }
+        boolean res = false;
+        for (Integer integer : integers) {
+            res |= help(a - integer, !f);
+        }
+        return res;
+    }
+
+    boolean isSq(int i) {
+        int sqrt = (int) Math.sqrt(i);
+        return sqrt * sqrt == i;
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution().isPathCrossing("NES"));
-
     }
 
 }
