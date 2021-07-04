6ace6c7c98c81b74eecf346e9a3c4b71621b832b
Zakl <1015849735@qq.com>
2021-07-04 12:13:59 +0800

    feat: 补充快速幂算法

--- src/main/java/algorithm/Quick_pow.java
+++ src/main/java/algorithm/Quick_pow.java
@@ -5,6 +5,7 @@ public class Quick_pow {
     /**
      * 快速幂算法
      * 求a的b次方
+     *
      * @param a
      * @param b
      * @return
@@ -20,9 +21,10 @@ public class Quick_pow {
     long powWithMod(int a, long b) {
         if (b == 0) return 1;
         long res = powWithMod(a, b / 2);
-        if ((b & 1) == 0) res = res * res;
-        else {
-            res = ((res * res)%mod) * a;
+        if ((b & 1) == 0) {
+            res = res * res;
+        } else {
+            res = ((res * res) % mod) * a;
         }
         return res % mod;
     }
